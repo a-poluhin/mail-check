cd99c6164d8f4923097083919df74cc501e2d4cd
Andrey Poluhin <andrey.poluhin@fullstack-development.com>
2022-09-30 23:45:40 +0700

    add part translations

---
+++ js/internalization.js
@@ -0,0 +1,69 @@
+const defaultLocale = "en";
+
+let locale;
+
+let translations = {};
+
+getValueByString = function(o, s) {
+  s = s.replace(/\[(\w+)\]/g, '.$1'); // convert indexes to properties
+  s = s.replace(/^\./, '');           // strip a leading dot
+  var a = s.split('.');
+  for (var i = 0, n = a.length; i < n; ++i) {
+      var k = a[i];
+      if (k in o) {
+          o = o[k];
+      } else {
+          return;
+      }
+  }
+  return o;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setLocale(defaultLocale);
+  bindLocaleSwitcher(defaultLocale);
+});
+
+async function setLocale(newLocale) {
+  if (newLocale === locale) return;
+  const newTranslations =
+    await fetchTranslationsFor(newLocale);
+  locale = newLocale;
+  translations = newTranslations;
+  translatePage();
+}
+
+async function fetchTranslationsFor(newLocale) {
+  const response = await fetch(`locales/${newLocale}/translation.json`, {
+    headers: {
+      'Content-Security-Policy': 'sandbox',
+    }
+  });
+  return await response.json();
+}
+
+function translatePage() {
+  document
+    .querySelectorAll("[data-i18n-key]")
+    .forEach(translateElement);
+}
+
+function translateElement(element) {
+  const key = element.getAttribute("data-i18n-key");
+  const translation = getValueByString(translations, key);
+  
+  if (element.tagName.toLowerCase() === 'input') {
+    element.value = translation;
+  } else {
+    element.innerText = translation;
+  }
+}
+
+function bindLocaleSwitcher(initialValue) {
+  const switcher =
+    document.querySelector("[data-i18n-switcher]");
+  switcher.value = initialValue;
+  switcher.onchange = (e) => {
+    setLocale(e.target.value);
+  };
+}
