7a1a0f19661003665213cc0eed7ef0d3c27f4836
Andrey Poluhin <andrey.poluhin@fullstack-development.com>
2022-11-30 22:35:03 +0700

    add find by track;

--- js/findByTrack.js
+++ js/findByTrack.js
@@ -1,17 +1,19 @@
 const searchButton = document.querySelector('.search-button');
 const searchInput = document.getElementById('search-input');
+const searchResult = document.getElementById('serach-result');
 
 const searchTracker = () => {
   const trackNumber = searchInput && searchInput.value
   if (trackNumber) {
-    // const url = `https://gdeposylka.ru/api/v4/tracker/detect/${trackNumber}`;
-    const url2 = `http://localhost:3000/find-track/${trackNumber}`;
+    const url = `http://localhost:3000/get-carriers/${trackNumber}`;
+
+    const child = searchResult.getElementsByTagName('table')[0];
+    if (child) searchResult.removeChild(child);
 
     fetch(
-      url2,
+      url,
       {
         method: "GET",
-        // mode: 'no-cors',
         headers: {
           "Access-Control-Allow-Origin": "*",
           'Content-Type': 'application/json',
@@ -20,8 +22,48 @@ const searchTracker = () => {
     ).then((response) => {
       return response.json();
     }).then((data) => {
-      const resultData = data;
-      console.log('resultData: ', resultData)
+      const resultData = JSON.parse(data);
+      if (resultData && resultData.length > 0) {
+        const url2 = `http://localhost:3000/find-by-track/${resultData[0].code}/${trackNumber}`;
+
+        fetch(
+          url2,
+          {
+            method: "GET",
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              'Content-Type': 'application/json',
+            }
+          }
+        ).then((response) => {
+          return response.json();
+        }).then((data) => {
+          const resultData = JSON.parse(data);
+          console.log('resultData: ', resultData);
+          
+          const date = new Date(resultData.createdAt).toLocaleString('ru-RU');
+          searchResult.innerHTML = `
+            <table cellpadding="10">
+              <tr>
+                <td>Вес, кг</td>
+                <td>${resultData.weight}</td>
+              </tr>
+              <tr>
+                <td>Получатель</td>
+                <td>${resultData.attributes.recipient}</td>
+              </tr>
+              <tr>
+                <td>Адресовано</td>
+                <td>${resultData.attributes.destinationAddress}</td>
+              </tr>
+              <tr>
+                <td>Добавлена на сайт</td>
+                <td>${date}</td>
+              </tr>
+            </table>
+          `;
+        })
+      }
     })
   }
 }
--- server/node-request.js
+++ server/node-request.js
@@ -1,4 +1,3 @@
-const http = require("http");
 const https = require("https");
 
 const cors = require('cors')
@@ -18,7 +17,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/find-track/:trackId', async (req, res) => {
+app.get('/get-carriers/:trackId', async (req, res) => {
   const trackId = req.params.trackId;
 
   var options = {
@@ -40,42 +39,39 @@ app.get('/find-track/:trackId', async (req, res) => {
       res.json(err);
     })
     response.on('data', function (data) {
-      console.log("body: " + data);
+      console.log("httpsreq body: " + data);
       res.json(data);
     });
-    response.on('end', function() {
-      console.log("ok");
-      res.send('ok');
-    })
   });
-
   httpsreq.end();
+})
 
-  // var options = {
-  //   host: 'api.trackru.ru',
-  //   port: 80,
-  //   path: 'http://api.trackru.ru/v1/carriers/',
-  //   method: 'GET',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Api-Key': '8890bff2-2494-4213-8b6e-7a1282d478a3',
-  //   }
-  // };
+app.get('/find-by-track/:carrier/:trackId', async (req, res) => {
+  const trackId = req.params.trackId;
+  const carrier = req.params.carrier;
 
-  // const httpreq = http.request(options, function (response) {
-  //   response.setEncoding('utf8');
-  //   response.on('error', (err) => {
-  //     console.log("err: ", err);
-  //   })
-  //   response.on('data', function (data) {
-  //     console.log("body: ", data);
-  //     res.json(data);
-  //   });
-  //   response.on('end', function() {
-  //     console.log('ok');
-  //     res.end();
-  //   })
-  // });
+  var options2 = {
+    host: 'moyaposylka.ru',
+    port: 443,
+    path: `https://moyaposylka.ru/api/v1/trackers/${carrier}/${trackId}`,
+    method: 'GET',
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      'Content-Type': 'application/json',
+      'X-Api-Key': '2e1e2dddae2c6506c06e1094ae3e6de0',
+    }
+  };
 
-  // httpreq.end();
+  const httpsreq2 = https.request(options2, function (response) {
+    response.setEncoding('utf8');
+    response.on('error', (err) => {
+      console.log("err");
+      res.json(err);
+    })
+    response.on('data', function (data) {
+      console.log("httpsreq2 body: " + data);
+      res.json(data);
+    });
+  });
+  httpsreq2.end();
 })
